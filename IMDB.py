05d6d4b026517ab94e71f71f50a09386c481fdd1
Amirlan-u <139662723+Amirlan-u@users.noreply.github.com>
2023-08-10 14:55:53 +0600

    Update IMDB.py

--- IMDB.py
+++ IMDB.py
@@ -1,19 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 df = pd.read_csv('IMDB-Movie-Data.csv')
-
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-
-#Зависит ли от даты выхода оцека фильма
-
-df["Year"].fillna(-1, inplace=True)
-
-print(round(df.groupby(by="Year")["Metascore", "Runtime (Minutes)"].mean(), 1))
-g = df[(df["Director"] == 'Woody Allen')]["Metascore"]
-df['Alexandre Aja'] = df["Director"] == 'Alexandre Aja'
-
-a = df[df['Alexandre Aja'] & df['Actors']]['Metascore']
-hn = df.groupby(by='Alexandre Aja')['Actors', 'Metascore']
-print(hn.tail(5))
-print(g)
+d = df.pivot_table(index='Year',
+                   values='Runtime (Minutes)',
+                   aggfunc=['max', 'mean'])
+d.plot(kind='bar', subplots=True)
+plt.show()
